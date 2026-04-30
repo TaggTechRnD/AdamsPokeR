ce49fb5a39fddf580ebcae3ccc5909abea977773
Adam T <a.taggart2016@gmail.com>
2026-04-30 15:45:06 -0400

    Added data objects in to work on. Added compare_strategies functionality

---
+++ R/compare_strategies.R
@@ -0,0 +1,21 @@
+compare_strategies <- function(sim, strategy_list) {
+
+  results <- lapply(names(strategy_list), function(name) {
+
+    dt <- strategy_list[[name]]
+
+    res <- sim %>%
+      apply_decision_table(dt) %>%
+      classify_outcomes() %>%
+      calculate_ev()
+
+    summary <- summarise_ev(res)
+
+    summary$strategy <- name
+
+    summary
+  })
+
+  dplyr::bind_rows(results) %>%
+    dplyr::select(strategy, dplyr::everything())
+}
---
+++ R/data_strategies.R
@@ -0,0 +1,17 @@
+#' Example Poker Strategies
+#'
+#' Predefined decision tables for testing strategy performance.
+#'
+#' @format A list of tibbles:
+#' \describe{
+#'   \item{dt_tight}{Conservative strategy}
+#'   \item{dt_base}{Baseline strategy}
+#'   \item{dt_loose}{Aggressive strategy}
+#' }
+"dt_tight"
+
+"dt_base"
+
+"dt_loose"
+
+"example_strategies"
